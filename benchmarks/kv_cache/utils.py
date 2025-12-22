390c48c7f7dd46764a82d3f45f48732a9d12ab0d
wang.yuqi <yuqi.wang@daocloud.io>
2025-12-22 15:47:23 +0800

    + test prefix_caching

---
+++ benchmarks/kv_cache/utils.py
@@ -0,0 +1,26 @@
+import random
+from typing import List, Union
+from vllm.tokenizers import get_tokenizer
+
+
+class TokenSampler:
+    def __init__(self, tokenizer, trust_remote_code=True):
+        if isinstance(tokenizer, str):
+            tokenizer = get_tokenizer(tokenizer, trust_remote_code=trust_remote_code)
+
+        self.tokenizer = tokenizer
+
+        vocab = tokenizer.get_vocab()
+        vocab = {k: v for k, v in vocab.items() if k not in tokenizer.all_special_ids}
+        vocab = list(vocab.values())
+
+        self.vocab = vocab
+
+    def random_sample(self, length: int, decode: bool = False) -> Union[List[int], str]:
+        prompt_token_ids = random.choices(self.vocab, k=length)
+
+        if not decode:
+            return prompt_token_ids
+
+        prompt = self.tokenizer.decode(prompt_token_ids)
+        return prompt
