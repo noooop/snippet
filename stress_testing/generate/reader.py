f31fcc111395d12684b86e509b8d19c55d1ee597
wang.yuqi <noooop@126.com>
2026-01-21 13:45:24 +0800

    update

---
+++ stress_testing/generate/reader.py
@@ -0,0 +1,34 @@
+
+
+
+import numpy as np
+
+
+def main(filename: str):
+    metrics = np.load(filename)
+    b, n = metrics.shape
+    sorted_metrics = np.sort(metrics, axis=0)
+
+    b10 = int(b * 0.1)
+    b20 = int(b * 0.2)
+    b25 = int(b * 0.25)
+    b50 = int(b * 0.5)
+    b75 = int(b * 0.75)
+    b80 = int(b * 0.8)
+    b90 = int(b * 0.9)
+
+    print("10% 20% 25% 25% 50% 75% 80% 90%")
+    for i, (a, b, c, d, e, f, g) in enumerate(zip(sorted_metrics[b10],
+                                                  sorted_metrics[b20],
+                                                  sorted_metrics[b25],
+                                                  sorted_metrics[b50],
+                                                  sorted_metrics[b75],
+                                                  sorted_metrics[b80],
+                                                  sorted_metrics[b90])
+                                              ):
+        print(i, a*1000, b*1000, c*1000, d*1000, e*1000, f*1000, g*1000)
+
+if __name__ == "__main__":
+    import sys
+    filename = sys.argv[1]
+    main(filename)
